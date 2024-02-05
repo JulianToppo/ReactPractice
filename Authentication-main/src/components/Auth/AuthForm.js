f608a5e3558abf0dcb8c9b846be9998fd5f5eae9
JulianToppo <juliantoppo95@gmail.com>
2024-02-05 12:32:44 +0530

    Firebase integration while sending requests

---
+++ Authentication-main/src/components/Auth/AuthForm.js
@@ -0,0 +1,82 @@
+import { useState, useRef } from "react";
+
+import classes from "./AuthForm.module.css";
+import { firebaseSignUpURL } from "../../utils/firebaseConstants";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setisLoading] = useState(false);
+
+  const email = useRef();
+  const password = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const onClickHandler = async (e) => {
+    try {
+      e.preventDefault();
+      if (isLogin) {
+      } else {
+        setisLoading(true);
+        const newUser = {
+          email: email.current.value,
+          password: password.current.value,
+          returnSecureToken: true,
+        };
+
+        const postData = await fetch(firebaseSignUpURL, {
+          method: "POST",
+          body: JSON.stringify(newUser),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        setisLoading(false); 
+        const data = await postData.json();
+        if (postData.ok) {
+          console.log("successfully added into firebase");
+         
+        }else{
+          console.log(data)
+          throw new Error(data.error.message)
+        }
+
+      }
+    } catch (error) {
+        
+      alert(error.message);
+    }
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input ref={email} type="email" id="email" required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input ref={password} type="password" id="password" required />
+        </div>
+        <div className={classes.actions}>
+          <button onClick={onClickHandler}>
+            {isLogin ? "Login" :( isLoading ? "Sending Request...." : "Signup")}
+          </button>
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
---
+++ Authentication-main/src/utils/firebaseConstants.js
@@ -0,0 +1,3 @@
+const API_KEY="AIzaSyBPdwdnhLk718_qxKhYSzyx1r5CZSCklvY"
+
+export const firebaseSignUpURL=`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`
