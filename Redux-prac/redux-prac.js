23a2d4fc6e72947264cbbf3666a336043bc6b241
JulianToppo <juliantoppo95@gmail.com>
2024-02-15 10:40:02 +0530

    React action for increment the value by 2 and decrement the value by 2

--- Redux-prac/redux-prac.js
+++ Redux-prac/redux-prac.js
@@ -1,11 +1,15 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = 0, action) => {
+const counterReducer = (state = 10, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return state + 5;
     case 'DECREMENT':
       return state - 1;
+    case 'INCREMENTBY2':
+      return state+2;
+    case 'DECREMENTBY2':
+      return state-2;
     default:
       return state;
   }
@@ -22,5 +26,7 @@ const unsubscribe = store.subscribe(handleStateChange);
 
 store.dispatch({ type: 'INCREMENT' }); 
 store.dispatch({type:'DECREMENT'});
+store.dispatch({type:'INCREMENTBY2'})
+store.dispatch({type: 'DECREMENTBY2'})
 
 
