9047c9e801bfde2b51917f1df9ae7bef67b2650c
JulianToppo <juliantoppo95@gmail.com>
2024-02-17 13:57:12 +0530

    Addition of notification bar on top wwhen data is sent to the database and useEffect function for fetching latest updated value

--- AdvanceRedux/01-starting-project/src/App.js
+++ AdvanceRedux/01-starting-project/src/App.js
@@ -1,13 +1,79 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useEffect } from "react";
+import { UseDispatch } from "react-redux";
+import React from "react";
+import Notification from "./components/UI/Notification";
+import { showNotification } from "./store/ui-slice";
 
+let isInitial = true;
 function App() {
+  const cart = useSelector((store) => store.cart);
+  const ui = useSelector((store) => store.ui);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        showNotification({
+          status: "pending",
+          title: "Sending... ",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://expensetracker-1b48b-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+
+      const responseData = await response.json();
+      dispatch(
+        showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if(isInitial){
+      isInitial=false
+      return
+    }
+
+    sendCartData().catch((err) => {
+      dispatch(
+        showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sent cart data failed!",
+        })
+      );
+    });
+  }, [cart]);
   return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
+    <React.Fragment>
+      {ui.notification && (
+        <Notification
+          status={ui.notification.status}
+          title={ui.notification.title}
+          message={ui.notification.message}
+        />
+      )}
+      <Layout>
+        <Cart />
+        <Products />
+      </Layout>
+    </React.Fragment>
   );
 }
 
---
+++ AdvanceRedux/01-starting-project/src/store/ui-slice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const uislice=createSlice({
+name:'ui',
+initialState:{
+    notification:null
+},
+reducers:{
+    showNotification:(state,action)=>{
+        state.notification={
+            status:action.payload.status,
+            title:action.payload.title,
+            message:action.payload.title
+        }
+    }
+}
+
+})
+
+export const {showNotification}= uislice.actions
+export default uislice.reducer
