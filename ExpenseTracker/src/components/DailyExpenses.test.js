8adab20d998fee074372813b6778dc6ca657cf2c
JulianToppo <juliantoppo95@gmail.com>
2024-02-19 15:10:03 +0530

    Addition of async test cases

--- ExpenseTracker/src/components/DailyExpenses.test.js
+++ ExpenseTracker/src/components/DailyExpenses.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
 import store from "../utils/store/centralStore";
 import { BrowserRouter } from "react-router-dom";
@@ -60,3 +60,39 @@ describe("Daily Expense test cases", () => {
     expect(addexpense).toBeInTheDocument();
   });
 });
+
+
+describe('async operations', () => { 
+  test('fetch expenses',async () => {
+    
+    window.fetch= jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => ({
+        sdsaf: {
+          category: 'Housing',
+          description: 'Something',
+          price: 1000
+        }
+      })
+    });
+    
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <DailyExpenses />
+        </BrowserRouter>
+      </Provider>
+    );
+
+
+    await waitFor(async() => {
+
+    const housing= await getByText('Housing')
+    expect(housing).toBeInTheDocument()
+
+    })
+
+  })
+  
+ })
